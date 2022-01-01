a7065dd2a0e8c5e9a480f4d976281e87eeb1433b
Ivanov_SV <Ivanov_SV3@yndex.ru>
2022-01-01 20:53:00 +0300

    Формирование массива и проверка условия на четность

--- Program.cs
+++ Program.cs
@@ -5,3 +5,16 @@ int[] array = new int[20];
 Random temp = new Random();
 int k = 0;
 
+Console.WriteLine("Исходный массив данных:");
+for (int i = 0; i < 20; i++)
+{
+    array[i] = temp.Next(1,99);
+    Console.Write(array[i]+" ");
+    if (array[i]%2 == 0)
+    {
+        k++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Количество четных элементов = "+k);
+Console.WriteLine("Массив из четных чисел:");
